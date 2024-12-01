1849ea290e9754d680198589d5432854556afe2f
MamoruDS <mamoruds.io@gmail.com>
2024-12-01 10:27:34 +0900

    add: lspconfig setup for luals

--- lua/dotfiles/plugins/lsp_config.lua
+++ lua/dotfiles/plugins/lsp_config.lua
@@ -1,3 +1,5 @@
+local lspconfig = require("lspconfig")
+
 local on_attach = function(client, bufnr)
   if client.server_capabilities.documentHighLightProvider then
     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -17,3 +19,29 @@ local on_attach = function(client, bufnr)
     })
   end
 end
+
+lspconfig.lua_ls.setup({
+  on_attach = on_attach,
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
+        return
+      end
+    end
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        version = "LuaJIT",
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+})
