f537cddc1ee90ae1584dd72105754611ac29821f
MamoruDS <mamoruds.io@gmail.com>
2024-12-01 10:18:50 +0900

    update: move hover to lsp client on_attach

--- lua/dotfiles/plugins/lsp_config.lua
+++ lua/dotfiles/plugins/lsp_config.lua
@@ -1,13 +1,19 @@
-vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.document_highlight()
-  end,
-})
+local on_attach = function(client, bufnr)
+  if client.server_capabilities.documentHighLightProvider then
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      pattern = "*",
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.document_highlight()
+      end,
+    })
 
-vim.api.nvim_create_autocmd("CursorMoved", {
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.clear_references()
-  end,
-})
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      pattern = "*",
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.clear_references()
+      end,
+    })
+  end
+end
