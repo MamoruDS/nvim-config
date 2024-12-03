adc826b806a04bcece030fd3b4600c4951710592
MamoruDS <mamoruds.io@gmail.com>
2024-12-03 16:08:20 +0900

    add: tag `no-native` for plugin treesitter

--- lua/dotfiles/plugins.lua
+++ lua/dotfiles/plugins.lua
@@ -114,7 +114,7 @@ M.plugins = {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    tags = { "default" },
+    tags = { "default", "no-native" },
     build = function()
       local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
       ts_update()
