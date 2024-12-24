9af33f5997dc0f2cd6d5d48fa6b22d58a84989a9
MamoruDS <mamoruds.io@gmail.com>
2024-12-25 01:40:07 +0900

    add: annotations for config

--- lua/dotfiles/config.lua
+++ lua/dotfiles/config.lua
@@ -2,6 +2,10 @@ local M = {}
 
 local utils = require("dotfiles.utils")
 
+--- @class dotfiles.config.default_opts
+--- @field appearances dotfiles.config.appearances
+--- @field lsp_clients {[string]: dotfiles.config.lsp_client}
+--- @field plugins dotfiles.config.plugins
 M.default_opts = {
   appearances = {
     dark_mode = true,
@@ -21,7 +25,29 @@ M.default_opts = {
   },
 }
 
-M.config = nil
+--- @class dotfiles.config.appearances
+--- @field dark_mode? boolean
+--- @field nerdfont? boolean
+--- @field transparent_bg? boolean
+
+--- @class dotfiles.config.lsp_client
+--- @field setup? function
+--- @field config? table
+
+--- @class dotfiles.config.plugins.load_tags
+--- @field positive? string[]
+--- @field negative? string[]
+
+--- @class dotfiles.config.plugins
+--- @field load_tags? dotfiles.config.plugins.load_tags
+--- @field extra_plugins? table
+--- @field fuzzy_finder? "fzf-lua"|"telescope"
+
+--- @class dotfiles.config
+--- @field appearances? dotfiles.config.appearances
+--- @field lsp_clients? {[string]: dotfiles.config.lsp_client}
+--- @field plugins? dotfiles.config.plugins
+M.config = {}
 
 function M.setup(opts)
   M.config = utils.merge_tables(M.default_opts, opts)
