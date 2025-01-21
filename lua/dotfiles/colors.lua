aac1c06ae31610b7c436a25d40d803e422909c4b
MamoruDS <mamoruds.io@gmail.com>
2025-01-21 16:45:18 +0900

    improve: apply hl via autocmd

--- lua/dotfiles/colors.lua
+++ lua/dotfiles/colors.lua
@@ -1,7 +1,13 @@
-if vim.opt.background:get() == "dark" then
-  vim.cmd([[
+local M = {}
+
+function M.apply()
+  if vim.opt.background == "dark" then
+    vim.cmd([[
         hi FloatBorder  ctermfg=8       guifg=#4f5661
         hi Search       guibg=#867647
         hi VertSplit    ctermfg=8       guifg=#4f5661
     ]])
+  end
 end
+
+return M
--- lua/dotfiles/init.lua
+++ lua/dotfiles/init.lua
@@ -2,6 +2,12 @@ local utils = require("dotfiles.utils")
 
 require("dotfiles.config").setup({})
 
+local colors = require("dotfiles.colors")
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = colors.apply,
+})
+
 utils.require("options")
 
 require("dotfiles.plugins").setup()
