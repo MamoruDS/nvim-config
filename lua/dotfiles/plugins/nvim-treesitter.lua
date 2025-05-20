3908cd69b5c2929fa75701564c3561c1678244a8
MamoruDS <mamoruds.io@gmail.com>
2025-05-20 11:53:00 +0900

    update: remove `ensure_installed` from treesitter config

--- lua/dotfiles/plugins/nvim-treesitter.lua
+++ lua/dotfiles/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-  ensure_installed = { "lua", "vim", "vimdoc" },
+  -- ensure_installed = {},
   sync_install = false,
   highlight = { enable = true },
   indent = {
