2a7db1f567d5b2268aa40a2b6ee84cef9a4916f1
MamoruDS <mamoruds.io@gmail.com>
2025-05-28 21:41:41 +0900

    add: types for highlight getter/setter

--- lua/dotfiles/utils/hl.lua
+++ lua/dotfiles/utils/hl.lua
@@ -3,12 +3,19 @@ local set_hl = vim.api.nvim_set_hl
 
 local hl = {}
 
+--- @alias HighlightGetter fun(name: string): vim.api.keyset.highlight
+--- @alias HighlightSetter fun(name: string, opts: vim.api.keyset.highlight): nil
+
+--- @type HighlightGetter
 function hl.get(name)
   return get_hl(0, { link = false, name = name })
 end
 
+--- @type HighlightSetter
 function hl.set(name, opts)
   set_hl(0, name, vim.tbl_deep_extend("force", hl.get(name), opts))
 end
 
+--- @alias dotfiles.utils.hl { get: HighlightGetter, set: HighlightSetter }
+--- @return dotfiles.utils.hl
 return hl
